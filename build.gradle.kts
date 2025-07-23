5afbeebd47846dd6e2a91f7a7ca83152ddd0546e
Joel Cain <fcftreetop@gmail.com>
2025-07-22 19:10:08 -0500

    Window working

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,3 +17,27 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
+val lwjglVersion = "3.3.6"
+val jomlVersion = "1.10.8"
+val lwjglNatives = "natives-windows"
+
+dependencies {
+    implementation(platform("org.lwjgl:lwjgl-bom:$lwjglVersion"))
+
+    implementation("org.lwjgl", "lwjgl")
+    implementation("org.lwjgl", "lwjgl-assimp")
+    implementation("org.lwjgl", "lwjgl-glfw")
+    implementation("org.lwjgl", "lwjgl-nfd")
+    implementation("org.lwjgl", "lwjgl-openal")
+    implementation("org.lwjgl", "lwjgl-opengl")
+    implementation("org.lwjgl", "lwjgl-stb")
+    implementation ("org.lwjgl", "lwjgl", classifier = lwjglNatives)
+    implementation ("org.lwjgl", "lwjgl-assimp", classifier = lwjglNatives)
+    implementation ("org.lwjgl", "lwjgl-glfw", classifier = lwjglNatives)
+    implementation ("org.lwjgl", "lwjgl-nfd", classifier = lwjglNatives)
+    implementation ("org.lwjgl", "lwjgl-openal", classifier = lwjglNatives)
+    implementation ("org.lwjgl", "lwjgl-opengl", classifier = lwjglNatives)
+    implementation ("org.lwjgl", "lwjgl-stb", classifier = lwjglNatives)
+    implementation("org.joml", "joml", jomlVersion)
+}
